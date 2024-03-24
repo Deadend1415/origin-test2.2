f0e7c1ef27c5252623751dd4ad99a83e38af3846
Deadend1415 <158297952+Deadend1415@users.noreply.github.com>
2024-03-24 17:14:23 +0100

    test4

---
+++ test 4/verify2fa.php
@@ -0,0 +1,25 @@
+<?php
+$host = "localhost";
+$db_user = "root";
+$db_psw = "";
+$db_name = "test";
+
+$connessione = mysqli_connect($host,$db_user,$db_psw,$db_name);
+
+
+    //if (!isset($id_utente)){$id_utente = $_POST["id_utente"];}
+    if (!isset($fa)){$fa = $_POST["fa"];}
+    if(isset($_POST['var'])) $var=$_POST['var'];
+
+$query = "select * from tbl_test4 where 2FA and 2FA_EXPIRE > CURRENT_TIMESTAMP";
+$output = mysqli_query($connessione,$query);
+
+if (count($output) === 0) {
+    //TODO: Inserire gestione di login fallito
+    exit("fallito");
+} else {
+    //TODO: Inserire gestione di login eseguito correttamente
+    exit("successo");
+}
+
+?>
